fa6c9e6f98dfbbacbd0bdda1ad4b64f21f62bdf1
BizhuWu <wubizhu0505@qq.com>
2025-09-01 10:42:36 +0800

    Update eval_t2m.py

--- eval_t2m.py
+++ eval_t2m.py
@@ -16,16 +16,14 @@ from torch import cuda
 
 if __name__ == "__main__":
 
-    args = option.get_args_parser()
-
-
+    parser = option.get_args_parser()
 
     # set hyperparameters
-    parser = argparse.ArgumentParser(description="Train on the Text-to-Motion task.")
     parser.add_argument("--model_name", type=str, default="./t2m-ft-from-t5-base/checkpoint-300000/", help="Trained model name or directory")
     parser.add_argument("--logger_dir", type=str, default="./t2m-ft-from-t5-base/checkpoint-300000/", help="Directory to save test log")
     parser.add_argument("--prompt", type=str, default="Generate motion: ", help="Text-to-Motion Prompt")
-    user_args = parser.parse_args()
+
+    args = parser.parse_args()
 
 
 
@@ -70,14 +68,14 @@ if __name__ == "__main__":
 
 
     # set logger
-    logger = utils_model.get_test_logger(user_args.logger_dir, 'test_t2m_run.log')
+    logger = utils_model.get_test_logger(args.logger_dir, 'test_t2m_run.log')
     logger.info(json.dumps(vars(args), indent=4, sort_keys=True))
 
 
 
     # motion aware language model setting
-    tokenizer = T5Tokenizer.from_pretrained(user_args.model_name)
-    model = T5ForConditionalGeneration.from_pretrained(user_args.model_name)
+    tokenizer = T5Tokenizer.from_pretrained(args.model_name)
+    model = T5ForConditionalGeneration.from_pretrained(args.model_name)
 
     device = 'cuda' if cuda.is_available() else 'cpu'
     model = model.to(device)
@@ -100,7 +98,7 @@ if __name__ == "__main__":
             logger,
             tokenizer,
             eval_wrapper=eval_wrapper,
-            instruction=user_args.prompt,
+            instruction=args.prompt,
             max_new_tokens=200
         )
         fid.append(best_fid)
